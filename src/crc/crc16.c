a65462b94469adfb4eb6aeda42d14509525e704c
Random121 <59186475+Random121@users.noreply.github.com>
2026-01-13 20:56:05 -0800

    crc: add CRC-16/CCITT function

--- src/crc/crc16.c
+++ src/crc/crc16.c
@@ -1,1 +1,19 @@
 #include "srp/crc/crc.h"
+
+#include <stdint.h>
+
+uint16_t crc16_ccitt(const uint8_t *data, size_t length)
+{
+    uint16_t crc = CRC16_CCITT_INITIAL_VALUE;
+    uint8_t e;
+    uint8_t f;
+
+    for (; length > 0; length--) {
+        e = crc ^ *data;
+        data++;
+        f = e ^ (e << 4);
+        crc = (crc >> 8) ^ ((uint16_t)f << 8) ^ ((uint16_t)f << 3) ^ ((uint16_t)f >> 4);
+    }
+
+    return crc;
+}
