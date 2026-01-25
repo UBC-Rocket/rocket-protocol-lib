2e72eb94a7b2768e7b4dded24d6893f74ee3303d
Random121 <59186475+Random121@users.noreply.github.com>
2026-01-25 10:04:37 -0800

    crc: add CRC-16/CCITT function (#2)
    * crc: add CRC-16/CCITT function
    
    * crc: add null check
    
    * crc: add test for CRC-16/CCITT implementation
    
    * crc: fix incorrect interface library linking
    
    * crc: add additional documentation
    
    * crc: clarify algorithm name in doc comment
    
    * crc: remove unnecessary uint16 cast
    
    * clang-format: disable line break formatting for comments
    
    * Revert "crc: remove unnecessary uint16 cast"
    
    This reverts commit 2dd8190a979cae0f168589b6a18ec8095aa30276.

--- include/srp/crc/crc.h
+++ include/srp/crc/crc.h
@@ -4,6 +4,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
-uint16_t crc16_ccitt(const uint8_t *data, size_t size);
+#define CRC16_CCITT_INITIAL_VALUE (0x0000)
+#define CRC16_CCITT_RESIDUE (0x0000)
+
+uint16_t crc16_ccitt(const uint8_t *data, size_t length);
 
 #endif // SRP_CRC_H
